42b8372cdfec7f9a1a782637066b99fde750a0b9
karura <karurachandesu@gmail.com>
2019-12-19 20:56:07 +0100

    Commit 01

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject panelCredits;
+
+    private float velTransition = 10000f;
+
+    private bool credits = false;
+
+    private float posPanelCredit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posPanelCredit = panelCredits.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (credits && (transform.position.x +50) <panelCredits.transform.position.x)
+        {
+
+            panelCredits.transform.Translate(Vector3.left * Time.deltaTime * velTransition);
+
+        }
+
+        if (!credits && posPanelCredit > panelCredits.transform.position.x)
+        {
+
+            panelCredits.transform.Translate(Vector3.right * Time.deltaTime * velTransition);
+
+        }
+    }
+
+    public void Play()
+    {
+
+        SceneManager.LoadScene("Map");
+
+    }
+
+    public void Credits()
+    {
+
+        credits = true;
+
+    }
+
+    public void Back()
+    {
+
+        credits = false;
+
+    }
+
+    public void Exit()
+    {
+
+        Application.Quit();
+
+    }
+
+
+}
